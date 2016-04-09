f8bd1ac681d2bb82b04706e560a8d21b0138d3d3
noppelmax <noppelmax@googlemail.com>
2016-04-09 13:29:12 +0200

    Autoformat

--- src/Err.php
+++ src/Err.php
@@ -1,20 +1,23 @@
 <?php
-function err_auth_error($response, $msg){
-  $response = $response->withStatus(403);
-  $response = err_error($response, "auth_error", "Please login to use this feature!");
+function err_auth_error($response, $msg)
+{
+    $response = $response->withStatus(403);
+    $response = err_error($response, "auth_error", "Please login to use this feature!");
 
-  return $response;
+    return $response;
 }
 
-function err_general_error($response, $msg){
-  $response = err_error($response, "general_error", $msg);
-  return $response;
+function err_general_error($response, $msg)
+{
+    $response = err_error($response, "general_error", $msg);
+    return $response;
 }
 
-function err_error($response, $type, $msg){
-  $response->getBody()->write('{"error":"'.$type.'","msg":"'.$msg.'"}');
+function err_error($response, $type, $msg)
+{
+    $response->getBody()->write('{"error":"' . $type . '","msg":"' . $msg . '"}');
 
-  return $response;
+    return $response;
 }
 
 ?>
--- src/Util.php
+++ src/Util.php
@@ -1,12 +1,14 @@
 <?php
 
-function validateDate($date){
-  list($y, $m, $d) = array_pad(explode('-', $date, 3), 3, 0);
-  return ctype_digit("$y$m$d") && checkdate($m, $d, $y);
+function validateDate($date)
+{
+    list($y, $m, $d) = array_pad(explode('-', $date, 3), 3, 0);
+    return ctype_digit("$y$m$d") && checkdate($m, $d, $y);
 }
 
-function success($msg){
-  die('{"success":"' . $msg . '"}');
+function success($msg)
+{
+    die('{"success":"' . $msg . '"}');
 }
 
 
